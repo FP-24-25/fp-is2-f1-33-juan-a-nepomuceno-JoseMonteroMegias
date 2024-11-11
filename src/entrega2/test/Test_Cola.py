dead451688646d275a4897bd3b3dfb8a6b6536d0
JoseMonteroMegias <josmonmeg@alum.us.es>
2024-11-11 18:01:04 +0100

    3 tests

--- src/entrega2/test/Test_Cola.py
+++ src/entrega2/test/Test_Cola.py
@@ -1,5 +1,16 @@
-'''
-Created on 7 nov 2024
+from entrega2.tipos.Cola import *
 
-@author: Usuario
-'''
+#1
+
+cola = Cola.of()
+cola.add(23)
+cola.add(47)
+cola.add(1)
+cola.add(2)
+cola.add(-3)
+cola.add(4)
+cola.add(5)
+print(cola)
+
+elemento=cola.remove_all()
+print(elemento)
--- src/entrega2/tipos/Cola.py
+++ src/entrega2/tipos/Cola.py
@@ -18,19 +18,3 @@ class Cola(AgregadoLineal[E]):
         """Devuelve una representación en cadena de la cola."""
         elements_str = ', '.join(map(str, self._elements))
         return f"Cola({elements_str})"
-
-# Ejemplo de uso
-if __name__ == "__main__":
-    # Crear una cola de enteros
-    cola = Cola.of()
-    
-    # Añadir elementos a la cola
-    cola.add(10)
-    cola.add(20)
-    cola.add(30)
-    
-    print(cola)  # Salida: Cola(10, 20, 30)
-    
-    # Eliminar el primer elemento (el más antiguo) usando remove()
-    print("Elemento eliminado:", cola.remove())  # Salida: 10
-    print(cola)  # Salida: Cola(20, 30)
